a1a7e3cc7d79dffb7c76e1c50b5c7b6e443a80cb
yasminaguardia <yasmina.guardia@naranja.com>
2022-12-05 21:46:17 -0300

    se habilita swagger en el proyecto

--- src/test/java/com/educacionIT/alumnosApi/AlumnosApiApplicationTests.java
+++ src/test/java/com/educacionIT/alumnosApi/AlumnosApiApplicationTests.java
@@ -1,12 +1,13 @@
 package com.educacionIT.alumnosApi;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlumnosApiApplicationTests {
 
-	@Test
+	@Disabled
 	void contextLoads() {
 	}
 
