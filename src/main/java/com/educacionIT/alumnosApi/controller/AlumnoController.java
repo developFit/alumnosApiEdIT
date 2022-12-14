c6a89344da51e0ff857d5da8fcd155ba86dfb6ec
ejmoncerrat <em@smartheads.com.ar>
2022-12-14 20:46:59 -0300

    nueva configuracion

--- src/main/java/com/educacionIT/alumnosApi/controller/AlumnoController.java
+++ src/main/java/com/educacionIT/alumnosApi/controller/AlumnoController.java
@@ -1,5 +1,6 @@
 package com.educacionIT.alumnosApi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,14 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.educacionIT.alumnosApi.DTO.RequestAlumnoDTO;
 import com.educacionIT.alumnosApi.models.Alumno;
+import com.educacionIT.alumnosApi.service.AlumnoService;
 
 @RestController
 public class AlumnoController {
+	
+	@Autowired
+	private AlumnoService alumnoService;
 
 	@PostMapping("/alta")
 	//@ApiOperation(value = "crear alumnos")
 	public ResponseEntity<?> altaAlumno(@RequestBody RequestAlumnoDTO alumno){
-	
+	    this.alumnoService.altaAlumno(alumno);
 		return ResponseEntity.ok("create");
 	}
+	
+	//consulta de alumno por id
+	
+	//consulta de todos alumnos
+	
+	//modificacion de alumnos
+	
+	// delete alumnos
+	
+	//consulta por apellido de alumno
+	
+	//alumno domicilio va a ser uan entidad realcionada alumno  
 }
