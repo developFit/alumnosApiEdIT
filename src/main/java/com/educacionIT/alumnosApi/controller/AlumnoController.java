0b709a7717e1640cc90987108df50dd0954dbb93
Marcelo Cantarella <marcelocantarella@gmail.com>
2022-12-14 21:25:33 -0300

    Agregado de endpoints, en clase

--- src/main/java/com/educacionIT/alumnosApi/controller/AlumnoController.java
+++ src/main/java/com/educacionIT/alumnosApi/controller/AlumnoController.java
@@ -1,20 +1,40 @@
 package com.educacionIT.alumnosApi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educacionIT.alumnosApi.DTO.RequestAlumnoDTO;
 import com.educacionIT.alumnosApi.models.Alumno;
+import com.educacionIT.alumnosApi.service.AlumnoService;
 
 @RestController
+@RequestMapping("/alumnoapi")
 public class AlumnoController {
-
-	@PostMapping("/alta")
+	
+	@Autowired
+	private AlumnoService service;
+	
+	@PostMapping("/")
 	//@ApiOperation(value = "crear alumnos")
 	public ResponseEntity<?> altaAlumno(@RequestBody RequestAlumnoDTO alumno){
-	
+		this.service.altaAlumno(alumno);
 		return ResponseEntity.ok("created");
 	}
+	
+	@GetMapping("/")
+	public ResponseEntity<?> getAlumnos(){	
+		return ResponseEntity.ok(this.service.getAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getAlumnosID(@PathVariable("id") Integer id){	
+		return ResponseEntity.ok(this.service.getAlumno(id));
+	}
+	
 }
--- src/main/java/com/educacionIT/alumnosApi/service/AlumnoService.java
+++ src/main/java/com/educacionIT/alumnosApi/service/AlumnoService.java
@@ -1,6 +1,8 @@
 package com.educacionIT.alumnosApi.service;
 import com.educacionIT.alumnosApi.models.Alumno;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +26,26 @@ public class AlumnoService {
 		
 		this.alumnoRepository.save(alumnoNuevo);
 	}
+	
+	public List<Alumno> getAll(){
+		return this.alumnoRepository.findAll();
+	}
+	
+	public Alumno getAlumno(Integer id) {
+		return this.alumnoRepository.findById(id).get();
+	}
+	
+	//consulta de alumno por id
+	
+	//consulta de todos alumnos
+		
+	//modificacion de alumnos
+		
+	// delete alumnos
+		
+	//consulta por apellido de alumno
+		
+	//alumno domicilio va a ser uan entidad realcionada alumno
 
+	
 }
