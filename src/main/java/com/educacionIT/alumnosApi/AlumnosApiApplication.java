cf4deabfbf51af91bfd1444ae080f6753c4e94ec
Marcelo Cantarella <marcelocantarella@gmail.com>
2022-12-21 21:36:53 -0300

    Modificaciones hasta el break clase 12

--- src/main/java/com/educacionIT/alumnosApi/AlumnosApiApplication.java
+++ src/main/java/com/educacionIT/alumnosApi/AlumnosApiApplication.java
@@ -2,11 +2,12 @@ package com.educacionIT.alumnosApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-//@EnableSwagger2
+@EnableWebMvc
 public class AlumnosApiApplication {
 
 	public static void main(String[] args) {
